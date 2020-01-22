87a8e3422d092e6264dc4cda280a3f15caee5fec
Brett Miller <brett.miller@sportngin.com>
2020-01-22 13:25:37 -0600

    fix nav and title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Becca Neely`,
+    description: `Becca Neely Portfolio.`,
+    author: `Brett Miller`,
   },
   plugins: [
     `gatsby-plugin-sass`,
